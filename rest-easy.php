7429f691ddcf3cd45872733005472bd4feef3246
John <john@funkhaus.us>
2017-09-21 11:26:57 -0400

    Initial commit, meta info in place

---
+++ rest-easy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Reast Easy
+ * Plugin URI:  http://funkhaus.us/
+ * Description: Rest-ify your site with zero effort and powerful customization.
+ * Version:     1.0
+ * Author:      John Robson, Funkhaus
+ * Author URI:  http://funkhaus.us
+ */
+
+! defined( 'ABSPATH' ) and exit;
