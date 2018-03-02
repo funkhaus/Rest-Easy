cf2041448cb0fafe41f970bb71f008d4c76f2c37
John Robson <john@funkhaus.us>
2018-03-02 13:12:10 -0500

    Added transient caching to post and attachment serializers

--- serializers/attachment.php
+++ serializers/attachment.php
@@ -1,64 +1,75 @@
 <?php
     function rez_default_serialize_attachment( $attachment = null ){
+        $output = array();
+        $attachment = get_post($attachment);
+
         if ( !$attachment ) return null;
 
-        $output = array();
+        // set unique key for transient
+        $key = 'rez_serializer_att_' . $attachment->ID;
 
-        $attachment = get_post($attachment);
-        $sizes = array_merge(get_intermediate_image_sizes(), array('full'));
+        // only do the work if transient does not exist
+        if ( ! $output = get_transient($key) ) {
 
-        // save alt text
-        $alt_text = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
-        if(empty($alt_text)) {
-            // Try the caption if no alt text
-            $alt_text = trim(strip_tags( $attachment->post_excerpt ));
+            // get all sizes
+            $sizes = array_merge(get_intermediate_image_sizes(), array('full'));
+
+            // save alt text
+            $alt_text = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
+            if(empty($alt_text)) {
+                // Try the caption if no alt text
+                $alt_text = trim(strip_tags( $attachment->post_excerpt ));
+            }
+            if(empty($alt_text)) {
+                // Try the title if no caption
+                $alt_text = trim(strip_tags( $attachment->post_title ));
+            }
+
+            // Get all meta fields without leading underscore
+            $meta = get_post_meta( $attachment->ID, '', true );
+            $filtered_meta = array_filter( $meta, 'filter_leading_underscore', ARRAY_FILTER_USE_KEY );
+
+            // Save attachment data
+            $output['ID'] = $attachment->ID;
+            $output['title'] = get_the_title($attachment->ID);
+            $output['alt'] = $alt_text;
+            $output['caption'] = trim(strip_tags( $attachment->post_excerpt ));
+            $output['description'] = trim(strip_tags( $attachment->post_content ));
+            $output['meta'] = array_map(function($i){ return $i[0]; }, $filtered_meta);
+
+            // add image colors if FIC (https://github.com/funkhaus/funky-colors) is installed
+            if ( function_exists('get_primary_image_color') ){
+                $output['primaryColor'] = get_primary_image_color($attachment->ID);
+
+                // included for backwards compatibility
+                $output['primary_color'] = get_primary_image_color($attachment->ID);
+            }
+
+            if ( function_exists('get_second_image_color') ){
+                $output['secondaryColor'] = get_second_image_color($attachment->ID);
+
+                // included for backwards compatibility
+                $output['secondary_color'] = get_second_image_color($attachment->ID);
+            }
+
+            // build out sizes
+            foreach ( $sizes as $size ){
+                $img_data = wp_get_attachment_image_src($attachment->ID, $size);
+
+                $output['sizes'][$size] = array(
+                    'url'       => $img_data[0],
+                    'width'     => $img_data[1],
+                    'height'    => $img_data[2],
+                    'html'      => wp_get_attachment_image($attachment->ID, $size),
+                );
+            }
+
+            // Add post type
+            $output['postType'] = get_post_type($attachment);
+
+            // save as transient
+            set_transient($key, $output, 15);
         }
-        if(empty($alt_text)) {
-            // Try the title if no caption
-            $alt_text = trim(strip_tags( $attachment->post_title ));
-        }
-
-        // Get all meta fields without leading underscore
-        $meta = get_post_meta( $attachment->ID, '', true );
-        $filtered_meta = array_filter( $meta, 'filter_leading_underscore', ARRAY_FILTER_USE_KEY );
-
-        // Save attachment data
-        $output['ID'] = $attachment->ID;
-        $output['title'] = get_the_title($attachment->ID);
-        $output['alt'] = $alt_text;
-        $output['caption'] = trim(strip_tags( $attachment->post_excerpt ));
-        $output['description'] = trim(strip_tags( $attachment->post_content ));
-        $output['meta'] = array_map(function($i){ return $i[0]; }, $filtered_meta);
-
-        // add image colors if FIC (https://github.com/funkhaus/funky-colors) is installed
-        if ( function_exists('get_primary_image_color') ){
-            $output['primaryColor'] = get_primary_image_color($attachment->ID);
-
-            // included for backwards compatibility
-            $output['primary_color'] = get_primary_image_color($attachment->ID);
-        }
-
-        if ( function_exists('get_second_image_color') ){
-            $output['secondaryColor'] = get_second_image_color($attachment->ID);
-
-            // included for backwards compatibility
-            $output['secondary_color'] = get_second_image_color($attachment->ID);
-        }
-
-        // build out sizes
-        foreach ( $sizes as $size ){
-            $img_data = wp_get_attachment_image_src($attachment->ID, $size);
-
-            $output['sizes'][$size] = array(
-                'url'       => $img_data[0],
-                'width'     => $img_data[1],
-                'height'    => $img_data[2],
-                'html'      => wp_get_attachment_image($attachment->ID, $size),
-            );
-        }
-
-        // Add post type
-        $output['postType'] = get_post_type($attachment);
 
         return $output;
     }
--- serializers/post.php
+++ serializers/post.php
@@ -6,61 +6,69 @@
     }
 
     function rez_default_serialize_post( $target_post = null ){
-
         $target_post = get_post( $target_post );
-
         if( ! $target_post ) return null;
 
-        // Get all meta fields without leading underscore
-        $meta = get_post_meta( $target_post->ID, '', true );
-        $filtered_meta = array_filter( $meta, 'filter_leading_underscore', ARRAY_FILTER_USE_KEY );
+        // set unique key for transient
+        $key = 'rez_serializer_post_' . $target_post->ID;
 
-        setup_postdata($target_post);
-        $excerpt = get_the_excerpt($target_post->ID);
-        if ( !$excerpt ) {
-            $content = get_post_field('post_content', $target_post->ID);
-            $trimmed = apply_filters('wp_trim_excerpt', $content);
-            $excerpt = apply_filters('the_excerpt', $trimmed);
-        }
+        // only do the work if transient does not exist
+        if ( ! $output = get_transient($key) ) {
 
-        $output = array(
-            'id'            => $target_post->ID,
-            'title'         => get_the_title($target_post),
-            'content'       => apply_filters('the_content', $target_post->post_content),
-            'excerpt'       => $excerpt,
-            'permalink'     => get_permalink($target_post),
-            'slug'          => $target_post->post_name,
-            'relativePath'  => rez_remove_siteurl( get_permalink($target_post) ),
-            'meta'          => array_map( 'reset', $filtered_meta ),
-            'date'          => get_the_date('U', $target_post->ID),
-            'isFront'       => get_option('page_on_front') == $target_post->ID,
-            'isBlog'        => get_option('page_for_posts') == $target_post->ID,
-            'isCategory'    => is_category()
-        );
+            // Get all meta fields without leading underscore
+            $meta = get_post_meta( $target_post->ID, '', true );
+            $filtered_meta = array_filter( $meta, 'filter_leading_underscore', ARRAY_FILTER_USE_KEY );
 
-        // if we have a featured image, add it
-        if ( $thumbnail_id = get_post_thumbnail_id( $target_post->ID ) ){
-            $output['featuredAttachment'] = apply_filters('rez_serialize_object', get_post($thumbnail_id));
-        }
-
-        // Add all attached media
-        $attached_media = get_attached_media('image', $target_post->ID);
-        $output['attachedMedia'] = array();
-        foreach( $attached_media as $single_attached_media ){
-            $output['attachedMedia'][] = apply_filters('rez_serialize_object', $single_attached_media);
-        }
-
-        // Add terms
-        $taxonomy_names = get_post_taxonomies($target_post->ID);
-        foreach($taxonomy_names as $taxonomy_name) {
-            $terms = wp_get_post_terms( $target_post->ID, $taxonomy_name);
-            if( !empty($terms) ) {
-                $output['terms'][$taxonomy_name] = $terms;
+            setup_postdata($target_post);
+            $excerpt = get_the_excerpt($target_post->ID);
+            if ( !$excerpt ) {
+                $content = get_post_field('post_content', $target_post->ID);
+                $trimmed = apply_filters('wp_trim_excerpt', $content);
+                $excerpt = apply_filters('the_excerpt', $trimmed);
             }
-        }
 
-        // Add post type
-        $output['postType'] = get_post_type($target_post);
+            $output = array(
+                'id'            => $target_post->ID,
+                'title'         => get_the_title($target_post),
+                'content'       => apply_filters('the_content', $target_post->post_content),
+                'excerpt'       => $excerpt,
+                'permalink'     => get_permalink($target_post),
+                'slug'          => $target_post->post_name,
+                'relativePath'  => rez_remove_siteurl( get_permalink($target_post) ),
+                'meta'          => array_map( 'reset', $filtered_meta ),
+                'date'          => get_the_date('U', $target_post->ID),
+                'isFront'       => get_option('page_on_front') == $target_post->ID,
+                'isBlog'        => get_option('page_for_posts') == $target_post->ID,
+                'isCategory'    => is_category()
+            );
+
+            // if we have a featured image, add it
+            if ( $thumbnail_id = get_post_thumbnail_id( $target_post->ID ) ){
+                $output['featuredAttachment'] = apply_filters('rez_serialize_object', get_post($thumbnail_id));
+            }
+
+            // Add all attached media
+            $attached_media = get_attached_media('image', $target_post->ID);
+            $output['attachedMedia'] = array();
+            foreach( $attached_media as $single_attached_media ){
+                $output['attachedMedia'][] = apply_filters('rez_serialize_object', $single_attached_media);
+            }
+
+            // Add terms
+            $taxonomy_names = get_post_taxonomies($target_post->ID);
+            foreach($taxonomy_names as $taxonomy_name) {
+                $terms = wp_get_post_terms( $target_post->ID, $taxonomy_name);
+                if( !empty($terms) ) {
+                    $output['terms'][$taxonomy_name] = $terms;
+                }
+            }
+
+            // Add post type
+            $output['postType'] = get_post_type($target_post);
+
+            // save transient
+            set_transient($key, $output, 15);
+        }
 
         return $output;
     }
